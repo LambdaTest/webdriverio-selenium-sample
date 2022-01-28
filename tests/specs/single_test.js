2e6580e0cce40b90d90122e03a0a9e691311f4a0
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-01-28 09:24:20 +0000

    pause function added

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -2,11 +2,11 @@ const assert = require('assert');
 
 describe('Google Search Function', () => {
   it('can find search results', () => {
-    browser.url('https://www.google.com/ncr');
+    browser.url('https://www.google.co.in/');
     const input = $('[name="q"]');
     input.setValue('test123');
-
     const title = browser.getTitle();
+    browser.pause(10000);
     assert.equal(title, 'Google');
   });
 });
