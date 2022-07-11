413deb05b2b99f33541d22b8582d9917c94738e6
Mukesh Rana <mukeshr@Admins-MacBook-Pro.local>
2022-07-11 18:35:41 +0530

    remove console.log

--- tests/specs/multiple/multitest1.js
+++ tests/specs/multiple/multitest1.js
@@ -5,7 +5,6 @@ describe('Google Search Function', () => {
     await browser
       .url('https://www.google.com/ncr')
       const prompt = await $('[id="L2AGLb"]'); // consent popup is coming for other location which needs to be accepted to proceed
-      console.log("hellllo",prompt)
       if(prompt.elementId)
         await prompt.click();
       const input = await $('[name="q"]');
