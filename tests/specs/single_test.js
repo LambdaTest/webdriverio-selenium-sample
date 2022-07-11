1bdfcd5e4dc88aac00a8c40bc96498b0d56dcc19
Mukesh Rana <mukeshr@Admins-MacBook-Pro.local>
2022-07-11 18:35:14 +0530

    fix tests

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -1,12 +1,15 @@
 const assert = require('assert');
 
 describe('Google Search Function', () => {
-  it('can find search results', () => {
-    browser.url('https://www.google.co.in/');
-    const input = $('[name="q"]');
-    input.setValue('test123');
-    const title = browser.getTitle();
-    browser.pause(10000);
+  it('can find search results', async () => {
+    await browser.url('https://www.google.co.in/');
+    const prompt = await $('[id="L2AGLb"]'); // consent popup is coming for other location which needs to be accepted to proceed
+    if(prompt.elementId)
+      await prompt.click();
+    const input = await $('[name="q"]');
+    await input.setValue('test123');
+    const title = await browser.getTitle();
+    await browser.pause(10000);
     assert.equal(title, 'Google');
   });
 });
